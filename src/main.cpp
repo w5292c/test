1db1612f4ea185d47024eb1c830ad3a830b21702
Alexander Chumakov <w5292c@gmail.com>
2014-12-15 12:58:16 +0300

    Added map test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "testbase.h"
 
+#include <QMap>
 #include <QDebug>
 #include <QCoreApplication>
 
@@ -15,6 +16,8 @@
     \endlist
 */
 
+QMap<int, QString> TheMap;
+
 int main(int argc, char **argv)
 {
   QCoreApplication app(argc, argv);
@@ -22,6 +25,14 @@ int main(int argc, char **argv)
   TestBase test;
   test.setValue(123);
   qDebug() << "main: " << test.getValue();
+
+  TheMap.insert(0, "This is the first element");
+  TheMap.insert(1, "This one is to be updated");
+  TheMap.insert(9, "Some more information is here, 9th element");
+  TheMap.insert(5, "Some element in the middle");
+  TheMap.insert(4, "The 4th element");
+  TheMap.insert(1, "Here is the 1st updated element");
+  qDebug() << "Here is the map:" << TheMap;
 
   return 0;
 }
